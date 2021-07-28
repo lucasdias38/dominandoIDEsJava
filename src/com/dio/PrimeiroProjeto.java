abb14cf028f0f0741d98053384e12a2122fd3d3f
lucasdias38 <lucas_rodrigues3838@hotmail.com>
2021-07-28 20:36:54 -0300

    first commit

---
+++ src/com/dio/PrimeiroProjeto.java
@@ -0,0 +1,21 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroProjeto {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+		Livro livro = new Livro();
+		System.out.println(gato);
+		System.out.println(livro);	
+		
+	}
+
+}
+
+
+class Livro{
+	private String nome;
+	private String npag;
+}
