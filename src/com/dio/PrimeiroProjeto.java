63d5aa37b357eb2a5101565e9493719a8df092f4
Lucas Rodrigues Dias <82161185+lucasdias38@users.noreply.github.com>
2021-07-28 20:41:09 -0300

    Update PrimeiroProjeto.java

--- src/com/dio/PrimeiroProjeto.java
+++ src/com/dio/PrimeiroProjeto.java
@@ -3,17 +3,13 @@ package com.dio;
 import com.dio.model.Gato;
 
 public class PrimeiroProjeto {
-
 	public static void main(String[] args) {
 		Gato gato = new Gato();
 		Livro livro = new Livro();
 		System.out.println(gato);
 		System.out.println(livro);	
-		
 	}
-
 }
-
 
 class Livro{
 	private String nome;
